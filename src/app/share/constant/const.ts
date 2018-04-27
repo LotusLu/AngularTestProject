dfc8b1514f475ffa8b4869e01c7be452b63f4d94
Lotus <popo21423@gmail.com>
2018-04-27 21:14:44 +0800

    FINAL

--- src/app/share/constant/const.ts
+++ src/app/share/constant/const.ts
@@ -11,9 +11,14 @@ export class Const {
     public static readonly LOGIN_USER: string = 'loginUser';
     public static readonly USER_TYPE: string = 'userType';
     public static readonly ADMIN_ROLE_CODE: string = 'admin';
-    public static readonly AUTH_URL: string = 'http://192.168.8.109:18081/';
-    public static readonly BACK_END_URL: string = 'http://192.168.8.109:65006/';
-    public static readonly SOCKET_BACK_END_URL: string = 'http://192.168.8.109:9092/';
+    //測試
+    // public static readonly AUTH_URL: string = 'http://192.168.8.109:18081/';
+    // public static readonly BACK_END_URL: string = 'http://192.168.8.109:65006/';
+    // public static readonly SOCKET_BACK_END_URL: string = 'http://192.168.8.109:9092/';
+    //正式
+    public static readonly AUTH_URL: string = 'http://192.168.148.169:18081/';
+    public static readonly BACK_END_URL: string = 'http://192.168.148.169:65006/';
+    public static readonly SOCKET_BACK_END_URL: string = 'http://192.168.148.169:9092/';
 
     public static readonly AUTH_ACCOUNT: string = 'ts-client';
     public static readonly AUTH_PASSWORD: string = 'ts-secret';
